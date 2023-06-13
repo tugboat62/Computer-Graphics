e0890792b01bb0e5237aca30b0aca7e8fb225604
tugboat62 <mujadded1998@gmail.com>
2023-06-13 20:08:56 +0600

    clock assignment added modified

--- clock.cpp
+++ clock.cpp
@@ -7,8 +7,9 @@
 GLdouble l = 0.4, bob = 0.05, r = 0.4, sr = 0.3;
 GLdouble l_ind = 0.06, s_ind = 0.02;
 GLdouble sec_hand = 0.2, min_hand = 0.15, hr_hand = 0.1;
-double theta = -60, dtheta = -60/100;
-double sa = 0, ma = -180, ha = -90;
+double theta = -90.0, dtheta = -60.0/100;
+double sa = 90, ma = 90, ha = -180;
+bool sec = false;
 
 void drawCircle(GLdouble r, GLdouble cx, GLdouble cy) {
     glBegin(GL_LINE_LOOP);  // All vertices form a single loop of single pixel width
@@ -33,7 +34,7 @@ void drawSphere(GLdouble r, GLdouble cx, GLdouble cy) {
 
 void drawPendulum() {
     drawSphere(bob, l * cos(theta * M_PI / 180), l * sin(theta * M_PI/180));
-    drawSphere(.02, 0, 0);
+    drawSphere(.03, 0, 0);
     glLineWidth(10);
     glBegin(GL_LINES);
         glVertex2d(0, 0); // Anchor point at the top
@@ -79,33 +80,47 @@ void display()
     glLoadIdentity(); // Reset Modelview matrix
 
     glLineWidth(.005);
+    
     drawFrame();
-
     drawPendulum();
+
     float sx = sec_hand * cos(sa/180*M_PI);
     float sy = r+bob + sec_hand * sin(sa/180*M_PI);
     float mx = min_hand * cos(ma/180*M_PI);
     float my = r+bob + min_hand * sin(ma/180*M_PI);
     float hx = hr_hand * cos(ha/180*M_PI);
     float hy = r+bob + hr_hand * sin(ha/180*M_PI);
+
+    //Second hand
     glLineWidth(0.5);
     drawLine(0, r+bob, sx, sy);
+    // Minute hand
     glLineWidth(2);
     drawLine(0, r+bob, mx, my);
+    // Hour hand
     glLineWidth(4); 
     drawLine(0, r+bob, hx, hy);
+
     glFlush();
 }
 
-void timerListener(int value) {
-    if (theta >= -60 || theta <= -120) 
+void timerListener1(int value) {
+    if (theta >= -60.0 || theta <= -120.0){
         dtheta = -dtheta;
+        sa = (int)(sa-6)%360;
+    }
     theta += dtheta;
-    sa -= 6/100;
-    ma -= 1/1000;
-    ha -= 1/60000;
+    ma -= 1.0/1000;
+    ha -= 1.0/60000;
+    // std::cout << sa << " " << ma << " " << ha << std::endl;
     glutPostRedisplay();
-    glutTimerFunc(10, timerListener, 0);
+    glutTimerFunc(10, timerListener1, 0);
+}
+
+void timerListener2(int value) {
+    sa -= 6.0;
+    glutPostRedisplay();
+    glutTimerFunc(1000, timerListener2, 0);
 }
 
 int main(int argc, char **argv)
@@ -115,12 +130,7 @@ int main(int argc, char **argv)
     glutInitWindowPosition(0, 100);
     glutCreateWindow("OpenGL Setup Test");
     glutDisplayFunc(display);
-    // glutIdleFunc(idle);
-    // glutKeyboardFunc(keyboard);
-    // glutSpecialFunc(special);
-    // glClearColor(1, 1, 1, 0);
-    glutTimerFunc(10, timerListener, 0);
-    // glutReshapeFunc(reshape);
+    glutTimerFunc(10, timerListener1, 0);
     glutMainLoop();
 
     return 0;
